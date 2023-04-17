6008b6c8a0edace5f007a336599d8e85cae5633b
dominic-g <dominicnjoroge1@gmail.com>
2023-04-17 13:55:37 -0400

    success page

---
+++ success.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/css/index.css" />
+    <link rel="stylesheet" href="./assets/css/success.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;400;500;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<main>
+		<section id="hero">
+			<header>
+				<nav class="navbar navbar-expand-sm navbar-light bg-light">
+				  <a class="navbar-brand" href="#"> <img src="./assets/img/logo.png" alt="Instant Cash offer for House"></a>
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				    <span class="navbar-toggler-icon"></span>
+				  </button>
+
+				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+				    <ul class="navbar-nav mr-auto">
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">How it Works</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">Reviews</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">Learn More</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">Contact</a>
+				      </li>
+				    </ul>
+				  </div>
+				</nav>
+			</header>
+			<div class="content">
+				<div class="container-fluid">
+					<div class="row text-center">
+						<h1>Congratulations!</h1>
+						<p>You're One Step Closer to Selling Your Home with Us</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		
+
+		<section id="message" class="container-fluid">
+
+			<div class="container-fluid">
+				<div class="row title">
+					<p>Our Team Will Review Your Information and Get Right Back!</p>
+				</div>
+
+				
+			</div>
+			
+		</section>
+
+		
+		<footer>
+			<section class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12 col-md-4 footer-img">
+						<img src="./assets/img/footer logo.png" alt="Footer Logo">
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 txt-cen">
+						<p>We are a business located in different parts of the US. Our team is working remotely from Atlanta.</p>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-4 txt-cen">
+						<p>Disclaimer: Final offer dependent on condition of property and your local agent.</p>
+					</div>
+				</div>
+			</section>
+		</footer>
+	</main>
+	<script type="text/javascript" src="./assets/js/main.js"></script>
+</body>
+</html>
